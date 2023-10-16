96e7671c1cffdedca5dbc46d4e351dc1761f2f3d
mathias8dev <kalipemathias.pro@gmail.com>
2023-10-16 06:49:27 +0200

    chore: Move of MultiplePermissionHelperScope interface to a separate file

--- PermissionHelper/src/main/java/com/mathias8dev/permissionhelper/permission/PermissionHelperScope.kt
+++ PermissionHelper/src/main/java/com/mathias8dev/permissionhelper/permission/PermissionHelperScope.kt
@@ -16,29 +16,3 @@ interface PermissionHelperScope {
     )
 }
 
-
-interface MultiplePermissionHelperScope {
-
-    /**
-     * Return the corresponding [PermissionState] corresponding to the provided permissions
-     */
-    fun getPermissionState(permissions: List<Permission>): List<Pair<Permission, PermissionState>>
-
-    /**
-     * Return the corresponding [PermissionState] corresponding to the provided permissionManifestKeys
-     */
-    fun getPermissionStateByKeys(permissionManifestKeys: List<String>): List<Pair<Permission, PermissionState>>
-
-    /**
-     * Used to launch permissions. Permissions are launched according to the defined launch strategy
-     */
-    suspend fun launchPermission(): List<Pair<Permission, PermissionState>>
-
-    /**
-     * Provide [PermissionLaunchStrategy] to the [MultiplePermissionHelperScope] to use when
-     * permissions are launched.
-     */
-    fun launchStrategy(
-        strategyProvider: (permissions: Collection<Permission>) -> PermissionLaunchStrategy?
-    )
-}
